d65d59d77787b77e0763fc7184c7bd27141922fa
leewp14 <lee.wp14@gmail.com>
2020-05-06 01:07:42 +0800

    galaxys2-common: remove AdvancedDisplay, SamsungDoze
    Unnecessary packages. Doze is already enabled in overlay.
    
    Change-Id: I228c091e586ddfc40a5934fd4109ffe9e83be8b5

--- common.mk
+++ common.mk
@@ -82,7 +82,6 @@ PRODUCT_PACKAGES += \
 
 # Audio Packages
 PRODUCT_PACKAGES += \
-    AdvancedDisplay \
     audio.primary.exynos4 \
     audio.a2dp.default \
     audio.r_submix.default \
@@ -150,9 +149,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Services
 PRODUCT_PROPERTY_OVERRIDES += \
     config.disable_atlas=true
-
-PRODUCT_PACKAGES += \
-    SamsungDoze
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
